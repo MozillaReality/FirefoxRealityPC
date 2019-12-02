ddfd9214220e5eb689357029bf1daf9a7f04e071
Philip Lamb <phil.git@eden.net.nz>
2019-12-02 13:25:20 +1300

    Update webexts and capture fix in https://bugzilla.mozilla.org/show_bug.cgi?id=1599846.

--- Source/web_extensions/webcompat_vimeo/background.js
+++ Source/web_extensions/webcompat_vimeo/background.js
@@ -2,7 +2,7 @@
 
 const CUSTOM_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.21 (KHTML, like Gecko) Version/9.2 Safari/602.1.21';
 
-var targets = [
+const targets = [
         "https://vimeo.com/*",
         "https://player.vimeo.com/video/*"
       ];
@@ -17,5 +17,5 @@ function rewriteUserAgentHeader(e) {
 }
 
 browser.webRequest.onBeforeSendHeaders.addListener(rewriteUserAgentHeader,
-                                          {urls: targets},
-                                          ["blocking", "requestHeaders"]);
+  {urls: targets},
+  ["blocking", "requestHeaders"]);
