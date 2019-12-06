5633db36e10604f95969ab8482d4194b70f42db0
Patrick O'Shaughnessey <patrick@patchedreality.com>
2019-12-06 11:19:22 -0600

    Experiment with loading scene with only an overlay in it that loads the Mushroom Crystal Cave scene asynchronously at startup - appears to eliminate the majority of flicker.

---
+++ Source/FirefoxRealityUnity/Assets/Scripts/FxRLoadEnvironment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR;
+
+public class FxRLoadEnvironment : MonoBehaviour
+{
+    private AsyncOperation LoadingOperation;
+
+    [SerializeField] protected SteamVR_Overlay LoadingOverlay;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+       yield return new WaitForSeconds(1f);
+       LoadingOperation = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+    }
+
+    private void Update()
+    {
+        if (LoadingOperation != null && LoadingOperation.isDone)
+        {
+            LoadingOverlay.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
