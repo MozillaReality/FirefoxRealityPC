54ad35d1c83388a8989c9d144f74fb9266ee88cb
Patrick O'Shaughnessey <patrick@patchedreality.com>
2019-12-10 17:46:40 -0600

    Disable the controllers and laser pointer while loading, as it is distracting and glitchy looking to have them to have them flashing on/off and jumping around during load

--- Source/FirefoxRealityUnity/Assets/Scripts/FxRLoadEnvironment.cs
+++ Source/FirefoxRealityUnity/Assets/Scripts/FxRLoadEnvironment.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class FxRLoadEnvironment : MonoBehaviour
 {
     private AsyncOperation LoadingOperation;
 
     [SerializeField] protected SteamVR_Overlay LoadingOverlay;
+    [SerializeField] private List<GameObject> Hands;
+
     // Start is called before the first frame update
     IEnumerator Start()
     {
        yield return new WaitForSeconds(1f);
        LoadingOperation = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+       foreach (var hand in Hands)
+       {
+           hand.SetActive(false);
+       }
     }
 
     private void Update()
@@ -21,6 +29,10 @@ public class FxRLoadEnvironment : MonoBehaviour
         {
             LoadingOverlay.gameObject.SetActive(false);
             gameObject.SetActive(false);
+            foreach (var hand in Hands)
+            {
+                hand.SetActive(true);
+            }
         }
     }
 }
