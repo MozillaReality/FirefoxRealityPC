d2cddbd0fb195e203d6081d4d984159ea5e11b0f
Patrick O'Shaughnessey <patrick@patchedreality.com>
2019-12-20 16:09:26 -0600

    Added a reflection probe to show reflections on the water

--- Source/FirefoxRealityUnity/Assets/Scripts/FxREnvironment.cs
+++ Source/FirefoxRealityUnity/Assets/Scripts/FxREnvironment.cs
@@ -1,20 +1,27 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
 
 public class FxREnvironment : MonoBehaviour
 {
     [SerializeField] private Material SkyboxMaterial;
     [SerializeField] private AmbientMode AmbientLightSource;
-    [SerializeField]  [ColorUsage(true, true)] private Color AmbientLightColor;
+    [SerializeField] [ColorUsage(true, true)] private Color AmbientLightColor;
+    [SerializeField] private List<ReflectionProbe> ReflectionProbs;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         RenderSettings.skybox = SkyboxMaterial;
         RenderSettings.ambientMode = AmbientLightSource;
         RenderSettings.ambientLight = AmbientLightColor;
+        yield return new WaitForSeconds(.5f);
+        foreach (var reflectionProbe in ReflectionProbs)
+        {
+            reflectionProbe.RenderProbe();
+        }
+        
     }
 
 }
