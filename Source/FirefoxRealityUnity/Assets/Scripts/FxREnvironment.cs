79c5855d5ca4fea8e75496332caa244659095eb6
Patrick O'Shaughnessey <patrick@patchedreality.com>
2019-12-20 13:50:31 -0600

    Add Offworld environment, and make it the default Add code to allow switching of the environment, although no UI as of yet to switch environments

---
+++ Source/FirefoxRealityUnity/Assets/Scripts/FxREnvironment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class FxREnvironment : MonoBehaviour
+{
+    [SerializeField] private Material SkyboxMaterial;
+    [SerializeField] private AmbientMode AmbientLightSource;
+    [SerializeField]  [ColorUsage(true, true)] private Color AmbientLightColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning(">>>> Loading environment skybox settings");
+        RenderSettings.skybox = SkyboxMaterial;
+        RenderSettings.ambientMode = AmbientLightSource;
+        RenderSettings.ambientLight = AmbientLightColor;
+    }
+
+}
---
+++ Source/FirefoxRealityUnity/Assets/Scripts/FxREnvironmentSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FxREnvironmentSwitcher : MonoBehaviour
+{
+    [SerializeField] private Transform EnvironmentParent;
+    [SerializeField] private List<FxREnvironment> EnvironmentPrefabs;
+
+    private FxREnvironment CurrentEnvironment;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchEnvironment(0);
+    }
+
+    // Update is called once per frame
+    void SwitchEnvironment(int environmentIndex)
+    {
+        if (CurrentEnvironment != null)
+        {
+            Destroy(CurrentEnvironment);
+        }
+        CurrentEnvironment = Instantiate(EnvironmentPrefabs[environmentIndex]
+            , EnvironmentParent.transform.position
+            , EnvironmentParent.transform.rotation
+            , EnvironmentParent);
+    }
+}
